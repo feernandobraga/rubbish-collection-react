ea16a09cf19069dad6c4e87ed385ccceae962c2f
Fernando <fernando.bragga@gmail.com>
2020-06-08 20:16:43 +1000

    add leaflet map

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Marker } from "react-leaflet";
 
 import "./styles.css";
 import logo from "../../assets/logo.svg";
@@ -49,6 +50,14 @@ const CreatePoint = () => {
             <h2>Address</h2>
             <span>Click on the map to select the address</span>
           </legend>
+
+          <Map center={[-37.7443007, 145.0155988]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-37.7443007, 145.0155988]}></Marker>
+          </Map>
 
           <div className="field-group">
             <div className="field">
